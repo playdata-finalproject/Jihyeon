c70de41a1f6b37cca50fd2e42d45517fed560daa
Jihyeonmun <jhmoon1994@gmail.com>
2021-02-19 11:09:00 +0900

    reference delete

--- reference/포모스_files/wp_ad_300_js.php
+++
@@ -1,6 +0,0 @@
-
-(function() {
-     
-			document.writeln(' <ifr' + 'ame frameborder="0" width="300" height="250"  '); 
-		   document.writeln(' src="https://stab3.clickmon.co.kr/ad_img/300.php?PopAd_A=1054209%5E%5E&v_type=&r_type=1&o_type=1&cm_r_url=https%3A%2F%2Fwww.fomos.kr%2F&re_id=98682010_20210218201701&ifrm=ok" scrolling="no"></ifr' + 'ame>');     
-	 })();   
